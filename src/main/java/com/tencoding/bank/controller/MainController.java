e4c18e6ad81089d857abed42697c4768c37a7b7e
Maleeang <dltmdgh757@naver.com>
2023-09-14 16:14:02 +0900

    layout header, footer 분리

---
+++ src/main/java/com/tencoding/bank/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.tencoding.bank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	//주소 설계
+	//GET localhost:80/main-page
+	@GetMapping("main-page")
+	public String mainPage() {
+		return "layout/main";
+	}
+}
