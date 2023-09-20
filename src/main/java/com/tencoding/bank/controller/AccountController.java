60ceecbb72ede481b8c58edc43712ac884535a95
Maleeang <dltmdgh757@naver.com>
2023-09-20 14:01:40 +0900

    입출금 기능

--- src/main/java/com/tencoding/bank/controller/AccountController.java
+++ src/main/java/com/tencoding/bank/controller/AccountController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tencoding.bank.dto.DepositFormDto;
 import com.tencoding.bank.dto.SaveFormDto;
+import com.tencoding.bank.dto.WithdrawFromDto;
 import com.tencoding.bank.handler.exception.CustomRestfullException;
 import com.tencoding.bank.handler.exception.UnAuthorizedException;
 import com.tencoding.bank.repository.model.Account;
@@ -84,14 +86,74 @@ public class AccountController {
 	//http://localhost/account/withdraw
 	@GetMapping("/withdraw")
 	public String withdraw() {
+		//1.인증 검사
+		User user = (User)session.getAttribute(Define.PRINCIPAL);
+		if(user == null) {
+			throw new UnAuthorizedException("로그인 먼저 해요", HttpStatus.UNAUTHORIZED);
+		}
 		return "account/withdraw";
+	}
+	
+	@PostMapping("/withdraw")
+	public String withdrawProc(WithdrawFromDto withdrawFromDto) {
+		
+		//1.인증검사
+		User user = (User)session.getAttribute(Define.PRINCIPAL);
+		if(user == null) {
+			throw new UnAuthorizedException("로그인 먼저 해요", HttpStatus.UNAUTHORIZED);
+		}
+		
+		//2.유효성 검사
+		if(withdrawFromDto.getAmount() == null) {
+			throw new CustomRestfullException("금액을 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+		if(withdrawFromDto.getAmount() <= 0) {
+			throw new CustomRestfullException("0원 이하일 수 없습니다", HttpStatus.BAD_REQUEST);
+		}
+		if(withdrawFromDto.getWAccountNumber() == null || withdrawFromDto.getWAccountNumber().isEmpty()) {
+			throw new CustomRestfullException("출금 계좌 번호를 입력 하시오", HttpStatus.BAD_REQUEST);
+		}
+		if(withdrawFromDto.getWAccountPassword() == null || withdrawFromDto.getWAccountPassword().isEmpty()) {
+			throw new CustomRestfullException("출금 계좌 비밀번호를 입력 하시오", HttpStatus.BAD_REQUEST);
+		}
+		//3.서비스
+		accountService.updateAccountWithdraw(withdrawFromDto, user.getId());
+		return "redirect:/account/list";
+		
 	}
 	
 	//입금 페이지
 	//http://localhost/account/deposit
 	@GetMapping("/deposit")
 	public String deposit() {
+		//1.인증검사
+		User user = (User)session.getAttribute(Define.PRINCIPAL);
+		if(user == null) {
+			throw new UnAuthorizedException("로그인 먼저 해요", HttpStatus.UNAUTHORIZED);
+		}
 		return "account/deposit";
+	}
+	
+	@PostMapping("/deposit")
+	public String depositProc(DepositFormDto depositFormDto) {
+		//1.인증검사
+		User user = (User)session.getAttribute(Define.PRINCIPAL);
+		if(user == null) {
+			throw new UnAuthorizedException("로그인 먼저 해요", HttpStatus.UNAUTHORIZED);
+		}
+		
+		//2.유효성 검사
+		if(depositFormDto.getAmount() == null) {
+			throw new CustomRestfullException("금액을 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+		if(depositFormDto.getAmount() <= 0) {
+			throw new CustomRestfullException("0원 이하일 수 없습니다", HttpStatus.BAD_REQUEST);
+		}
+		if(depositFormDto.getDAccountNumber() == null || depositFormDto.getDAccountNumber().isEmpty()) {
+			throw new CustomRestfullException("입금 계좌 번호를 입력 하시오", HttpStatus.BAD_REQUEST);
+		}
+		accountService.updateAccountDeposit(depositFormDto);
+		return "redirect:/account/list";
 	}
 	
 	//이체 페이지
--- src/main/java/com/tencoding/bank/service/AccountService.java
+++ src/main/java/com/tencoding/bank/service/AccountService.java
@@ -7,16 +7,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tencoding.bank.dto.DepositFormDto;
 import com.tencoding.bank.dto.SaveFormDto;
+import com.tencoding.bank.dto.WithdrawFromDto;
 import com.tencoding.bank.handler.exception.CustomRestfullException;
 import com.tencoding.bank.repository.interfaces.AccountRepository;
+import com.tencoding.bank.repository.interfaces.HistoryRepository;
 import com.tencoding.bank.repository.model.Account;
+import com.tencoding.bank.repository.model.History;
 
 @Service //IOC 대상 + 싱글톤 패턴으로 -> 스프링 컨테이너 메모리에 객체가 생성
 public class AccountService {
 	
 	@Autowired
 	private AccountRepository accountRepository;
+	@Autowired
+	private HistoryRepository historyRepository;
 	
 	@Transactional
 	public void createAccount(SaveFormDto saveFormDto, Integer principalId) {
@@ -40,6 +46,76 @@ public class AccountService {
 		List<Account> accountList;
 		accountList = accountRepository.findByUserId(principalId);
 		return accountList;
+	}
+
+	//출금 기능 로직
+	//1. 계좌 존재 여부 확인 -- select query
+	//2. 본인 계좌 여부 확인
+	//3. 계좌 비번 일치 확인
+	//4. 잔액 여부 확인
+	//5. 출금 처리 -- update query
+	//6. 거래내역 등록 -- insert query
+	//7. Transactional 처리
+	@Transactional
+	public void updateAccountWithdraw(WithdrawFromDto withdrawFromDto, Integer id) {
+		//1
+		Account accountEnitity = accountRepository.findByNumber(withdrawFromDto.getWAccountNumber());
+		if(accountEnitity == null) {
+			throw new CustomRestfullException("해당 계좌가 없습니다.", HttpStatus.BAD_REQUEST);
+		}
+		//2
+		if(accountEnitity.getUserId() != id) {
+			throw new CustomRestfullException("본인 계좌가 아닙니다.", HttpStatus.BAD_REQUEST);
+		}
+		//3
+		if(accountEnitity.getPassword().equals(withdrawFromDto.wAccountPassword) == false) {
+			throw new CustomRestfullException("계좌 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+		}
+		//4
+		if(accountEnitity.getBalance() < withdrawFromDto.amount) {
+			throw new CustomRestfullException("계좌 잔액이 부족합니다.", HttpStatus.BAD_REQUEST);
+		}
+		//5 모델 객체 상태 변경 --> 객체를 업데이트 쿼리에 던지기
+		accountEnitity.withdraw(withdrawFromDto.amount);
+		accountRepository.updateById(accountEnitity);
+		//6 History 객체 생성 --> insert
+		History history = new History();
+		history.setAmount(withdrawFromDto.amount);
+		history.setWBalance(accountEnitity.getBalance());
+		history.setDBalance(null);
+		history.setWAccountId(accountEnitity.getId());
+		history.setDAccountId(null);
+		int resultRowCount = historyRepository.insert(history);
+		if(resultRowCount != 1) {
+			throw new CustomRestfullException("정상 처리 되지 않았습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+
+	//입금 기능 로직
+	//1. 계좌 존재 여부 확인
+	//2. 입금 처리
+	//3. 거래 내역 등록
+	public void updateAccountDeposit(DepositFormDto depositFormDto) {
+		//1
+		Account accountEntity = accountRepository.findByNumber(depositFormDto.getDAccountNumber());
+		if(accountEntity == null) {
+			throw new CustomRestfullException("계좌번호가 없습니다.", HttpStatus.BAD_REQUEST);
+		}
+		//2
+		accountEntity.deposit(depositFormDto.amount);
+		accountRepository.updateById(accountEntity);
+		//3
+		History history = new History();
+		history.setAmount(depositFormDto.amount);
+		history.setDAccountId(accountEntity.getId());
+		history.setWAccountId(null);
+		history.setDBalance(accountEntity.getBalance());
+		history.setWBalance(null);
+		int resultRowCount = historyRepository.insert(history);
+		if(resultRowCount !=1) {
+			throw new CustomRestfullException("정상 처리 되지 않았습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 }
